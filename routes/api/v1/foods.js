e96aac54b8a8d923683f82489b15a7d86a82c69d
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-10-08 14:33:03 -0600

    Add show functionality for certain food to be returned

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -9,4 +9,15 @@ router.get('/', function(req, res, next) {
   })
 })
 
+router.get("/:id", function(req, res, next) {
+  res.setHeader("Content-Type", "application/json");
+  return foods.findOne({
+    where: {id: req.params.id},
+    attributes: ['id', 'name', 'calories']
+  })
+  .then(response => {
+    res.status(200).send(response)
+  })
+})
+
 module.exports = router;
