a4124ca3ac4cc7180a843bf70c4735972e6b2d77
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-10-09 20:45:28 -0600

    Add filter for attributes to be returned excluding created at and updated at for both meals index and show

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -1,12 +1,20 @@
 var express = require ('express')
 var router = express.Router()
 const meals = require("../../../models").Meal
+const models = require("../../../models")
 
 router.get("/", function(req, res, next) {
   res.setHeader("Content-Type", "application/json");
   return meals.findAll({
     attributes: ['id', 'name'],
-    include: 'foods'
+    include: [{
+      model: models.Food,
+      as: 'foods',
+      attributes: ['id', 'name', 'calories'],
+      through: {
+        attributes: []
+      }
+    }]
   }).then(results => {
     res.status(200).send(results)
   })
@@ -14,5 +22,29 @@ router.get("/", function(req, res, next) {
     console.log(error)
   })
 });
+
+router.get("/:id", function(req, res, next) {
+  res.setHeader("Content-Type", "application/json");
+  return meals.findByPk((req.params.id), {
+    attributes: ['id', 'name'],
+    include: [{
+      model: models.Food,
+      as: 'foods',
+      attributes: ['id', 'name', 'calories'],
+      through: {
+        attributes: []
+      }
+    }]
+  }).then(results => {
+    if (results) {
+      res.status(200).send(results)
+    } else {
+      res.status(404).send({message: "Meal cannot be found"})
+    }
+  })
+  .catch(error => {
+    res.status(500).send({error})
+  })
+})
 
 module.exports = router;
