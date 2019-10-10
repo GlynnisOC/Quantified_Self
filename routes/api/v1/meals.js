7864b6e2bc8fdba02744eb4c934fcac3fb82f2c3
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-09 22:05:18 -0600

    Add POST route for associating food and meals

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -1,6 +1,7 @@
 var express = require ('express')
 var router = express.Router()
 const meals = require("../../../models").Meal
+const foods = require("../../../models").Food
 
 router.get("/", function(req, res, next) {
   res.setHeader("Content-Type", "application/json");
@@ -14,5 +15,25 @@ router.get("/", function(req, res, next) {
     res.status(404).send({message: "No meals exist"})
   })
 });
+
+router.post('/:meal_id/foods/:food_id', function(req, res, next) {
+  res.setHeader("Content-Type", "application/json");
+  meals.findOne({where: {id: req.params.meal_id}})
+  .then(meal => {
+    foods.findOne({where: {id: req.params.food_id}})
+    .then(food => {
+      meal.addFood(food)
+      .then(response => {
+        res.status(201).send({message: `Successfully added ${food.name} to ${meal.name}`})
+      })
+    })
+    .catch(error => {
+      res.status(404).send({message: "Food ID does not exist"})
+    })
+  })
+  .catch(error => {
+    res.status(404).send({message: "Meal ID does not exist"})
+  })
+})
 
 module.exports = router;
